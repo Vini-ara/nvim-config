b41a9a0e475b96c6e25be55a5eb14650a863437a
vinicius araujo <vinicius.ar02@gmail.com>
2023-06-16 23:07:52 -0300

    nem sei o que mudei kkk

--- lua/bigu/packer.lua
+++ lua/bigu/packer.lua
@@ -22,7 +22,7 @@ return require('packer').startup(function(use)
             -- LSP Support
             { 'neovim/nvim-lspconfig' }, -- Required
             {
-                              -- Optional
+                -- Optional
                 'williamboman/mason.nvim',
                 run = function()
                     pcall(vim.cmd, 'MasonUpdate')
@@ -31,9 +31,9 @@ return require('packer').startup(function(use)
             { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },   -- Required
+            { 'hrsh7th/nvim-cmp' },     -- Required
             { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },   -- Required
+            { 'L3MON4D3/LuaSnip' },     -- Required
         }
     }
     use('prettier/vim-prettier')
@@ -42,5 +42,9 @@ return require('packer').startup(function(use)
         config = function()
             require('gitsigns').setup()
         end
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
 end)
